afe99228ed7724a6b921c64b028162e3af03f17a
Norbi1027 <sandornorbi7@gmail.com>
2023-09-12 08:54:41 +0200

    Add project files.

---
+++ app1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az első számot.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja mega második számot.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("A két szám összege:{0} ", c);
+            Console.ReadKey();
+        }
+    }
+}
