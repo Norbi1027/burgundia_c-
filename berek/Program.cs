a390989bb2e2ec027888350b693511956d701669
Norbi1027 <sandornorbi7@gmail.com>
2023-09-12 14:16:10 +0200

    kész

--- berek/Program.cs
+++ berek/Program.cs
@@ -9,13 +9,49 @@ namespace berek
 {
     internal class Program
     {
+        string bekertReszleg = "";
         static List <beker_class> adatok = new List <beker_class> ();
         static void Main(string[] args)
         {
             adatBeolvasas();
             feladat3();
+            feladat4 ();
+            feladat6();
+            feladat7 ();
             Console.WriteLine("Program vége");
             Console.ReadKey();
+        }
+
+        private static void feladat7()
+        {
+            Console.WriteLine($"7. Feladat: Statisztika");
+            adatok.GroupBy(x => x.Részleg1).ToList().ForEach(x => Console.WriteLine($"\t{x.Key} - {x.Count()} fő"));
+        }
+
+        public static void feladat6()
+        {
+            try
+            {
+                Console.Write("5. Feladat: Kérem a részleg nevét: ");
+                string bekertReszleg = Console.ReadLine();
+                int index = adatok.IndexOf(adatok.Where(x => x.Részleg1 == bekertReszleg).OrderByDescending(x => x.Bér1).First());
+                Console.WriteLine("6. Feladat: A legtöbbet kereső dolgozó a megadott részlegen");
+                Console.WriteLine($"\tNév: {adatok[index].Név1}");
+                Console.WriteLine($"\tNem: {adatok[index].Neme1}");
+                Console.WriteLine($"\tBelépés: {adatok[index].Belépés1}");
+                Console.WriteLine($"\tBér: {adatok[index].Bér1:### ###} Forint");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("6. Feladat: A megadott részleg nem létezik a cégnél!");
+            }
+        }
+
+
+        private static void feladat4()
+        {
+                Console.WriteLine($"{(double)adatok.Average(x => x.Bér1) / 1000:00.0} eFt");
+
         }
 
         private static void feladat3()
